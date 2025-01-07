decda09a3d95a9bd04b27f57c045e979a026e300
Johannes Brodwall <johannes@brodwall.com>
2025-01-07 12:56:05 +0100

    we are now sort of showing a map

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import {createRoot} from "react-dom/client";
+import {Map, View} from "ol";
+import TileLayer from "ol/layer/Tile";
+import {OSM} from "ol/source";
+
+const map = new Map({
+    layers: [ new TileLayer({ source: new OSM() }) ],
+    view: new View({center: [9.5, 59.9], zoom: 8})
+})
+
+function Application() {
+    const mapRef = useRef();
+    useEffect(() => {
+        map.setTarget(mapRef.current)
+    }, []);
+
+    return <div ref={mapRef}>Here is a Map in React</div>;
+}
 
 createRoot(document.getElementById("root")).render(
-    <div>Here is a Map in React</div>
+    <Application/>
 )
