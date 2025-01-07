f277188cc329024c2ab0fc7a2df13e670dda58fb
Johannes Brodwall <johannes@brodwall.com>
2025-01-07 09:58:03 +0100

    style municipalities

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -8,20 +8,26 @@ import {useGeographic} from "ol/proj";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import {GeoJSON} from "ol/format";
+import {Stroke, Style} from "ol/style";
 
 useGeographic();
+
+const municipalityLayer = new VectorLayer({
+    source: new VectorSource({ url: "/kws2100-kartbaserte-websystemer/geojson/kommuner.json", format: new GeoJSON() }),
+    style: new Style({
+        stroke: new Stroke({ color: "red", width: 2 })
+    })
+});
 
 const map = new Map({
     layers: [
         new TileLayer({source: new OSM()}),
-        new VectorLayer({
-            source: new VectorSource({ url: "/kws2100-kartbaserte-websystemer/geojson/kommuner.json", format: new GeoJSON() }),
-        }),
+        municipalityLayer,
         new VectorLayer({
             source: new VectorSource({ url: "/kws2100-kartbaserte-websystemer/geojson/vgs.json", format: new GeoJSON() }),
         }),
     ],
-    view: new View({center: [10.6, 59.9], zoom: 12})
+    view: new View({center: [10.6, 59.9], zoom: 10})
 })
 
 export function Application() {
