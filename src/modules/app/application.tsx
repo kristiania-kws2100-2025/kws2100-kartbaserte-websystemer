36f3abda4f030293f5b3335672aa9b55a36a9592
Johannes Brodwall <johannes@brodwall.com>
2025-01-07 09:52:26 +0100

    display a map with OpenLayers (fixed errors)

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -3,9 +3,14 @@ import {Map, View} from "ol";
 import TileLayer from "ol/layer/Tile";
 import {OSM} from "ol/source";
 
+import "ol/ol.css";
+import {useGeographic} from "ol/proj";
+
+useGeographic();
+
 const map = new Map({
     layers: [new TileLayer({source: new OSM()})],
-    view: new View({center: [10.6, 59.9], zoom: 6})
+    view: new View({center: [10.6, 59.9], zoom: 12})
 })
 
 export function Application() {
@@ -13,5 +18,5 @@ export function Application() {
     useEffect(() => {
         map.setTarget(mapRef.current);
     }, []);
-    return <div ref={mapRef}>I am a React map - for real!</div>;
+    return <div ref={mapRef}></div>;
 }
