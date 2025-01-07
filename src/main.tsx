ca5395252deff750c666a2c2521f6bf8d1cfb8fe
Johannes Brodwall <johannes@brodwall.com>
2025-01-07 13:29:05 +0100

    showing kommuner

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,22 @@ import {OSM} from "ol/source";
 import {useGeographic} from "ol/proj";
 
 import "ol/ol.css"
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import {GeoJSON} from "ol/format";
 
 useGeographic();
 
 const map = new Map({
-    layers: [ new TileLayer({ source: new OSM() }) ],
+    layers: [
+        new TileLayer({ source: new OSM() }),
+        new VectorLayer({
+            source: new VectorSource({
+                url: "/kws2100-kartbaserte-websystemer/geojson/kommuner.geojson",
+                format: new GeoJSON()
+            })
+        })
+    ],
     view: new View({center: [10.6, 59.9], zoom: 11})
 })
 
