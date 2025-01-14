fe2d78aa96a71a881f13893cf9a18a8cefe203dd
Johannes Brodwall <johannes@brodwall.com>
2025-01-14 18:21:26 +0100

    show a map

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,21 @@
 import { createRoot } from "react-dom/client";
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
+import { useGeographic } from "ol/proj";
 
-createRoot(document.getElementById("root")!).render(
-  <h1>Hello Map Application</h1>,
-);
+useGeographic();
+
+const map = new Map({
+  view: new View({ center: [10.8, 59.9], zoom: 13 }),
+  layers: [new TileLayer({ source: new OSM() })],
+});
+
+function Application() {
+  const mapRef = useRef<HTMLDivElement>(null);
+  useEffect(() => map.setTarget(mapRef.current!), []);
+  return <div ref={mapRef}></div>;
+}
+
+createRoot(document.getElementById("root")!).render(<Application />);
