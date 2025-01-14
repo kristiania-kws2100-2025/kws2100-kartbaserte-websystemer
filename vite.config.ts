4933b456b0c1b9b2c972df285318a2365f501375
Johannes Brodwall <johannes@brodwall.com>
2025-01-14 19:43:35 +0100

    add vite react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
+import viteReactPlugin from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/kws2100-kartbaserte-websystemer",
+  plugins: [viteReactPlugin()],
 });
