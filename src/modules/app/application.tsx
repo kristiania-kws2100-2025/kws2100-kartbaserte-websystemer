078ac64101fec7f7db94c27ed15f91f9db9eb613
Johannes Brodwall <johannes@brodwall.com>
2025-01-14 19:26:02 +0100

    Feature: Show my location on map

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -1,18 +1,24 @@
-import React, { useEffect, useRef } from "react";
-import { Map, View } from "ol";
+import React, { useEffect, useRef, useState } from "react";
+import { Feature, Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
 
 import "./application.css";
 import "ol/ol.css";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { Point } from "ol/geom";
 
 useGeographic();
 
-const map = new Map({
-  view: new View({ center: [10.8, 59.9], zoom: 13 }),
-  layers: [new TileLayer({ source: new OSM() })],
-});
+const userSource = new VectorSource();
+const layers = [
+  new TileLayer({ source: new OSM() }),
+  new VectorLayer({ source: userSource }),
+];
+const view = new View({ center: [10.8, 59.9], zoom: 13 });
+const map = new Map({ view, layers });
 
 function MapView() {
   const mapRef = useRef<HTMLDivElement>(null);
@@ -32,6 +38,35 @@ function ZoomToMeButton() {
   return <button onClick={handleClick}>Zoom to my location</button>;
 }
 
+function ShowMeCheckbox() {
+  const feature = new Feature();
+
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    if (checked) {
+      const number = navigator.geolocation.watchPosition(
+        ({ coords: { latitude, longitude } }) => {
+          feature.setGeometry(new Point([longitude, latitude]));
+        },
+      );
+      userSource.addFeature(feature);
+      return () => navigator.geolocation.clearWatch(number);
+    } else {
+      userSource.clear();
+    }
+  }, [checked]);
+
+  return (
+    <label>
+      <button onClick={() => setChecked((t) => !t)}>
+        <input type={"checkbox"} checked={checked} readOnly /> Show my location
+        in map
+      </button>
+    </label>
+  );
+}
+
 export function Application() {
   return (
     <>
@@ -40,6 +75,7 @@ export function Application() {
       </header>
       <nav>
         <ZoomToMeButton />
+        <ShowMeCheckbox />
       </nav>
       <MapView />
     </>
