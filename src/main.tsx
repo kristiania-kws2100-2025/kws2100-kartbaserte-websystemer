23584116d668b37431222307ffbdfecd9478305c
Johannes Brodwall <johannes@brodwall.com>
2025-01-14 18:22:10 +0100

    refactor: move map to separate file

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,5 @@
 import { createRoot } from "react-dom/client";
-import React, { useEffect, useRef } from "react";
-import { Map, View } from "ol";
-import TileLayer from "ol/layer/Tile";
-import { OSM } from "ol/source";
-import { useGeographic } from "ol/proj";
-
-useGeographic();
-
-const map = new Map({
-  view: new View({ center: [10.8, 59.9], zoom: 13 }),
-  layers: [new TileLayer({ source: new OSM() })],
-});
-
-function Application() {
-  const mapRef = useRef<HTMLDivElement>(null);
-  useEffect(() => map.setTarget(mapRef.current!), []);
-  return <div ref={mapRef}></div>;
-}
+import React from "react";
+import { Application } from "./modules/app/application";
 
 createRoot(document.getElementById("root")!).render(<Application />);
