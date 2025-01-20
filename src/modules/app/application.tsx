5bb2a7d4976d49cd18b3ecf27c015930e0688b07
Johannes Brodwall <johannes@brodwall.com>
2025-01-20 19:41:10 +0100

    StadiaMaps

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
-import { OSM } from "ol/source";
+import { StadiaMaps } from "ol/source";
 import { useGeographic } from "ol/proj";
 
 import "ol/ol.css";
@@ -9,7 +9,13 @@ import "ol/ol.css";
 useGeographic();
 
 const map = new Map({
-  layers: [new TileLayer({ source: new OSM() })],
+  layers: [
+    new TileLayer({
+      source: new StadiaMaps({
+        layer: "alidade_smooth_dark",
+      }),
+    }),
+  ],
   view: new View({
     center: [10.8, 59.9],
     zoom: 12,
