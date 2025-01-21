1346a8c5ea4789daa934e3260ab94db74eb457fd
Johannes Brodwall <johannes@brodwall.com>
2025-01-21 13:53:42 +0100

    show overlay when clicking on a school

--- src/modules/layers/schoolLayerCheckbox.tsx
+++ src/modules/layers/schoolLayerCheckbox.tsx
@@ -2,16 +2,16 @@ import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
 import { Layer } from "ol/layer";
-import React, { useEffect, useState } from "react";
-import { Map, MapBrowserEvent } from "ol";
+import React, { useEffect, useRef, useState } from "react";
+import { Map, MapBrowserEvent, Overlay } from "ol";
+import { FeatureLike } from "ol/Feature";
 
 const source = new VectorSource({
   url: "/kws2100-kartbaserte-websystemer/geojson/schools.geojson",
   format: new GeoJSON(),
 });
-const schoolLayer = new VectorLayer({
-  source,
-});
+const schoolLayer = new VectorLayer({ source });
+const overlay = new Overlay({});
 
 export function SchoolLayerCheckbox({
   setLayers,
@@ -21,6 +21,19 @@ export function SchoolLayerCheckbox({
   map: Map;
 }) {
   const [checked, setChecked] = useState(true);
+  const overlayRef = useRef<HTMLDivElement>(null);
+  const [selectedSchools, setSelectedSchools] = useState<FeatureLike[]>([]);
+
+  function handleClick(e: MapBrowserEvent<MouseEvent>) {
+    setSelectedSchools(map.getFeaturesAtPixel(e.pixel));
+    overlay.setPosition(e.coordinate);
+  }
+
+  useEffect(() => {
+    overlay.setElement(overlayRef.current!);
+    map.addOverlay(overlay);
+  }, []);
+
   useEffect(() => {
     if (checked) {
       setLayers((old) => [...old, schoolLayer]);
@@ -33,6 +46,10 @@ export function SchoolLayerCheckbox({
     <button onClick={() => setChecked((b) => !b)}>
       <input type={"checkbox"} checked={checked} />
       Show schools on map
+      <div ref={overlayRef}>
+        Clicked schools:{" "}
+        {selectedSchools.map((s) => s.getProperties().navn).join(", ")}
+      </div>
     </button>
   );
 }
