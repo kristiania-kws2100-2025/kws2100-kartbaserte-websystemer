c94a77bb1b2f51b9ae4ea8079665227c38a1f908
Johannes Brodwall <johannes@brodwall.com>
2025-01-21 12:59:34 +0100

    focus on our current position

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { createRoot } from "react-dom/client";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
@@ -10,18 +10,30 @@ import "./application.css";
 
 useGeographic();
 
-const map = new Map({
-  layers: [new TileLayer({ source: new OSM() })],
-  view: new View({ center: [10.8, 59.9], zoom: 12 }),
-});
+const view = new View({ center: [10.8, 59.9], zoom: 10 });
+const layers = [new TileLayer({ source: new OSM() })];
+const map = new Map({ layers, view });
 
 function Application() {
   const mapRef = useRef<HTMLDivElement>(null);
   useEffect(() => map.setTarget(mapRef.current!), []);
+
+  function handleClick() {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        view.animate({
+          center: [longitude, latitude],
+          zoom: 15,
+          duration: 500,
+        });
+      },
+    );
+  }
+
   return (
     <>
       <nav>
-        <button>Center on me</button>
+        <button onClick={handleClick}>Center on me</button>
       </nav>
       <main>
         <div ref={mapRef}></div>
