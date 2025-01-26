c64cf25fa2caac55572acd96bf5a16cd771549bf
Johannes Brodwall <johannes@brodwall.com>
2025-01-26 20:28:43 +0100

    convert base layer selection to useMemo

--- src/modules/app/baseLayerSelect.tsx
+++ src/modules/app/baseLayerSelect.tsx
@@ -1,34 +1,58 @@
 import TileLayer from "ol/layer/Tile";
 import { OSM, StadiaMaps } from "ol/source";
 import { Layer } from "ol/layer";
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useEffect, useMemo, useState } from "react";
 
 export const osmLayer = new TileLayer({ source: new OSM() });
-const layerOptions = {
-  osm: {
-    label: "OpenStreetMap",
-    layer: osmLayer,
-  },
-  stadia: {
-    label: "Stadia",
-    layer: new TileLayer({
-      source: new StadiaMaps({ layer: "alidade_smooth_dark" }),
-    }),
-  },
-} as const;
+
+type LayerOptions = Record<
+  "osm" | "stadia",
+  {
+    layer: Layer;
+    label: string;
+  }
+>;
 
 export function BaseLayerSelect({
   setBaseLayer,
 }: {
   setBaseLayer: (layer: Layer) => void;
 }) {
-  function handleChange(e: ChangeEvent<HTMLSelectElement>) {
-    const selectedLayer = e.target.value as keyof typeof layerOptions;
-    setBaseLayer(layerOptions[selectedLayer].layer);
-  }
+  const [selectedLayerValue, setSelectedLayerValue] =
+    useState<keyof LayerOptions>("osm");
+  const stadiaLayer = useMemo<Layer>(
+    () =>
+      new TileLayer({
+        source: new StadiaMaps({ layer: "alidade_smooth" }),
+      }),
+    [],
+  );
+
+  const layerOptions = useMemo<LayerOptions>(
+    () => ({
+      osm: {
+        label: "OpenStreetMap",
+        layer: osmLayer,
+      },
+      stadia: {
+        label: "Stadia",
+        layer: stadiaLayer,
+      },
+    }),
+    [],
+  );
+  const selectedLayer = useMemo(
+    () => layerOptions[selectedLayerValue],
+    [layerOptions, selectedLayerValue],
+  );
+  useEffect(() => setBaseLayer(selectedLayer.layer), [selectedLayer]);
 
   return (
-    <select onChange={handleChange}>
+    <select
+      onChange={(e) =>
+        setSelectedLayerValue(e.target.value as keyof LayerOptions)
+      }
+    >
       {Object.entries(layerOptions).map(([k, v]) => (
         <option key={k} value={k}>
           {v.label}
