fce33a99e385063249bf4dd3d993eb41554ec2b9
Johannes Brodwall <johannes@brodwall.com>
2025-01-28 12:51:47 +0100

    change between two background maps

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
-import { OSM } from "ol/source";
+import { OSM, StadiaMaps } from "ol/source";
 import { useGeographic } from "ol/proj";
 
 // Styling of OpenLayers components like zoom and pan controls
 import "ol/ol.css";
-import TileSource from "ol/source/Tile";
 
 import "./application.css";
 
@@ -20,12 +19,11 @@ const map = new Map({
   // The map will be centered on a position in longitude (x-coordinate, east) and latitude (y-coordinate, north),
   //   with a certain zoom level
   view: new View({ center: [10.8, 59.9], zoom: 10 }),
-  // map tile images will be from the Open Street Map (OSM) tile layer
-  layers: [new TileLayer({ source: new OSM() })],
 });
 
 // A functional React component
 export function Application() {
+  const [layers, setLayers] = useState([new TileLayer({ source: new OSM() })]);
   // `useRef` bridges the gap between JavaScript functions that expect DOM objects and React components
   const mapRef = useRef<HTMLDivElement | null>(null);
   // When we display the page, we want the OpenLayers map object to target the DOM object refererred to by the
@@ -33,6 +31,20 @@ export function Application() {
   useEffect(() => {
     map.setTarget(mapRef.current!);
   }, []);
+  useEffect(() => {
+    map.setLayers(layers);
+  }, [layers]);
+
+  function handleChange(e: ChangeEvent<HTMLSelectElement>) {
+    console.log(e.target.value);
+    if (e.target.value === "stadia") {
+      setLayers([
+        new TileLayer({ source: new StadiaMaps({ layer: "alidade_smooth" }) }),
+      ]);
+    } else {
+      setLayers([new TileLayer({ source: new OSM() })]);
+    }
+  }
 
   // This is the location (in React) where we want the map to be displayed
   return (
@@ -41,9 +53,9 @@ export function Application() {
         <h1>My map application</h1>
       </header>
       <nav>
-        <select>
-          <option>Open Street Map</option>
-          <option>Stadia</option>
+        <select onChange={handleChange}>
+          <option value={"osm"}>Open Street Map</option>
+          <option value={"stadia"}>Stadia</option>
         </select>
       </nav>
       <main>
