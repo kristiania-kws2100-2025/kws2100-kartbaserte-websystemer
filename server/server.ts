ce3c623d9e8a33c99edfd6d30f73de279a9d26b7
Johannes Brodwall <johannes@brodwall.com>
2025-02-04 13:32:12 +0100

    attempt to get geojson from postgis using hono

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,22 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+import pg from "pg";
+
+const postgresql = new pg.Pool({ user: "postgres" });
 
 const app = new Hono();
 app.get("/", async (c) => {
   return c.text("Hello somebody");
 });
-app.get("/kws2100-kartbaserte-websystemer/api/skoler", (c) => {
+app.get("/kws2100-kartbaserte-websystemer/api/skoler", async (c) => {
+  const result = await postgresql.query(
+    `select
+         skolenavn,
+         besoksadresse_besoksadresse_adressenavn as adresse,
+         posisjon::json as coordinates
+    from grunnskoler_3697913259634315b061b324a3f2cf59.grunnskole
+    `,
+  );
   return c.json({
     type: "FeatureCollection",
     crs: {
@@ -14,7 +25,14 @@ app.get("/kws2100-kartbaserte-websystemer/api/skoler", (c) => {
         name: "urn:ogc:def:crs:OGC:1.3:CRS84",
       },
     },
-    features: [],
+    features: result.rows.map(({ coordinates, ...properties }) => ({
+      type: "Feature",
+      properties,
+      geometry: {
+        type: "Point",
+        coordinates,
+      },
+    })),
   });
 });
 serve(app);
