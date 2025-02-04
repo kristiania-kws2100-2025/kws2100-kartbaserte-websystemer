3d903014b038176f98dd1e97eef1e3ac21453440
Johannes Brodwall <johannes@brodwall.com>
2025-02-04 13:26:14 +0100

    starting to make a geojson api

--- server/server.ts
+++ server/server.ts
@@ -5,4 +5,16 @@ const app = new Hono();
 app.get("/", async (c) => {
   return c.text("Hello somebody");
 });
+app.get("/kws2100-kartbaserte-websystemer/api/skoler", (c) => {
+  return c.json({
+    type: "FeatureCollection",
+    crs: {
+      type: "name",
+      properties: {
+        name: "urn:ogc:def:crs:OGC:1.3:CRS84",
+      },
+    },
+    features: [],
+  });
+});
 serve(app);
