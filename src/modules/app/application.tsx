f392a5d7a9058792ce6a4ee5f6e53ef7d2f40a82
Johannes Brodwall <johannes@brodwall.com>
2025-02-04 12:36:23 +0100

    show more layers (no it doesn't work yet)

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -5,12 +5,29 @@ import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
 
 import "ol/ol.css";
+import { GeoJSON } from "ol/format";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
 
 useGeographic();
 
+const osmLayer = new TileLayer({ source: new OSM() });
+const municipalityLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/geojson/kommuner.geojson",
+    format: new GeoJSON(),
+  }),
+});
+const schoolLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/geojson/skoler.geojson",
+    format: new GeoJSON(),
+  }),
+});
+
 const map = new Map({
   view: new View({ center: [10.8, 59.9], zoom: 8 }),
-  layers: [new TileLayer({ source: new OSM() })],
+  layers: [osmLayer, municipalityLayer, schoolLayer],
 });
 
 export function Application() {
