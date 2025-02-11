14089d0bc94918ffd9228db950ea5c8c52429b5c
Johannes Brodwall <johannes@brodwall.com>
2025-02-11 12:46:41 +0100

    second step for heroku: start script & use port from environment

--- server/server.ts
+++ server/server.ts
@@ -35,4 +35,8 @@ app.get("/api/skoler", async (c) => {
     ),
   });
 });
-serve(app);
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+serve({
+  fetch: app.fetch,
+  port,
+});
