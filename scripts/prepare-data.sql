02a555ba4f6e0642c88f8c37ed51b70045354873
Johannes Brodwall <johannes@brodwall.com>
2025-02-19 22:16:36 +0100

    define db:addresses scripts

--- scripts/prepare-data.sql
+++ scripts/prepare-data.sql
@@ -8,3 +8,16 @@ select kommunenavn,
        st_transform(st_simplify(omrade, 100), 3857) omrade_3857_simple,
        st_transform(omrade, 4326)                   omrade_4326
 from kommuner_4d2a1f720b994f11baaeae13ee600c8e.kommune;
+
+drop table if exists vegadresse;
+create table vegadresse
+as
+select adresseid,
+       adressetekst,
+       adressenavn,
+       bokstav,
+       nummer,
+       representasjonspunkt,
+       st_transform(representasjonspunkt, 4326) representasjonspunkt_4326,
+       st_transform(representasjonspunkt, 3857) representasjonspunkt_3857
+from matrikkelenvegadresse_71a47c657f3c4bbd83ebafaf8fb8a43e.vegadresse;
