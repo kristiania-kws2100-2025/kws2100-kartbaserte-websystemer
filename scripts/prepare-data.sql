ba8574f8f6d4c19468ef2f3ace6e8bae021f78b9
Johannes Brodwall <johannes@brodwall.com>
2025-02-25 00:22:41 +0100

    add index for vegadresse

--- scripts/prepare-data.sql
+++ scripts/prepare-data.sql
@@ -21,3 +21,6 @@ select adresseid,
        st_transform(representasjonspunkt, 4326) representasjonspunkt_4326,
        st_transform(representasjonspunkt, 3857) representasjonspunkt_3857
 from matrikkelenvegadresse_71a47c657f3c4bbd83ebafaf8fb8a43e.vegadresse;
+
+create index vegadresse_representasjonspunkt_3857_idx
+on vegadresse using GIST (representasjonspunkt_3857);
