a9836a75ee2dbd882de48cc008f7df61a0e58efc
Johannes Brodwall <johannes@brodwall.com>
2025-02-25 13:22:32 +0100

    Serve /api/kommuner from Postgis

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,38 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
+import pg from "pg";
+
 const app = new Hono();
-app.get("/api/kommuner", (c) => {
-  return c.json({});
+
+const connectionString = process.env.DATABASE_URL;
+
+const postgresql = connectionString
+  ? new pg.Pool({ connectionString, ssl: { rejectUnauthorized: false } })
+  : new pg.Pool({ user: "postgres" });
+
+app.get("/api/kommuner", async (c) => {
+  const query = await postgresql.query(`
+      select kommunenummer, kommunenavn, st_transform(omrade, 4326)::json geometry
+      from kommuner_4d2a1f720b994f11baaeae13ee600c8e.kommune
+  `);
+
+  return c.json({
+    type: "FeatureCollection",
+    crs: {
+      type: "name",
+      properties: {
+        name: "urn:ogc:def:crs:OGC:1.3:CRS84",
+      },
+    },
+    features: query.rows.map(
+      ({ kommunenavn, kommunenummer, geometry: { coordinates } }) => ({
+        type: "Feature",
+        geometry: { type: "MultiPolygon", coordinates },
+        properties: { kommunenummer, kommunenavn },
+      }),
+    ),
+  });
 });
 app.use("*", serveStatic({ root: "../dist" }));
 
