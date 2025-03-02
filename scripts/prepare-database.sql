09cc992c8e8c5e2430eaa5dafb8d4d02cde49940
Johannes Brodwall <johannes@brodwall.com>
2025-03-02 18:32:38 +0100

    import vegadresse

--- scripts/prepare-database.sql
+++ scripts/prepare-database.sql
@@ -7,3 +7,13 @@ select
     omrade,
     st_transform(omrade, 3857) omrade_3857
 from kommuner_4d2a1f720b994f11baaeae13ee600c8e.kommune;
+
+drop table if exists vegadresse;
+create table vegadresse
+as
+select
+    adresseid, adressekode, adressetekst,
+    adressenavn, nummer, bokstav, postnummer, poststed,
+    representasjonspunkt,
+    st_transform(representasjonspunkt, 3857) as representasjonspunkt_3857
+from matrikkelenadresse_49f3a8437d6443de8b4e042d78436756.vegadresse;
