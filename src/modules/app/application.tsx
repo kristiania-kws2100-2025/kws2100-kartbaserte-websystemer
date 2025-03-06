7e782e0fbd55767f3b0cffc21487c30fdfddf5db
Johannes Brodwall <johannes@brodwall.com>
2025-03-06 19:44:47 +0100

    (attempt at highlighting active schools) - fails to properly clone render feature

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import { Map, View } from "ol";
+import { Feature, Map, MapBrowserEvent, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
@@ -9,6 +9,10 @@ import { MVT } from "ol/format";
 import VectorTileLayer from "ol/layer/VectorTile";
 import VectorTileSource from "ol/source/VectorTile";
 import { Circle, Fill, Stroke, Style, Text } from "ol/style";
+import { FeatureLike } from "ol/Feature";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { Geometry } from "ol/geom";
 
 useGeographic();
 
@@ -17,18 +21,11 @@ const schoolLayer = new VectorTileLayer({
     url: "/api/schools/{z}/{x}/{y}",
     format: new MVT(),
   }),
-  style: (feature) => [
-    new Style({
-      text: new Text({
-        text: feature.getProperties().skolenavn,
-        offsetY: 20,
-        font: "15px sans-serif",
-      }),
-    }),
-    new Style({
+  style: function (feature: FeatureLike) {
+    return new Style({
       image: new Circle({
         radius: 8 + feature.getProperties().antallelever / 80,
-        stroke: new Stroke({ color: "black", width: 3 }),
+        stroke: new Stroke({ color: "black", width: 1 }),
         fill: new Fill({
           color:
             feature.getProperties().eierforhold === "Offentlig"
@@ -36,9 +33,10 @@ const schoolLayer = new VectorTileLayer({
               : "purple",
         }),
       }),
-    }),
-  ],
+    });
+  },
 });
+const activeSchools = new VectorSource<FeatureLike>();
 const map = new Map({
   view: new View({ center: [10.755, 59.915], zoom: 14 }),
   layers: [
@@ -56,13 +54,54 @@ const map = new Map({
         format: new MVT(),
       }),
     }),
+    new VectorLayer({
+      source: activeSchools,
+      style: (feature) => {
+        return [
+          new Style({
+            text: new Text({
+              text: feature.getProperties().skolenavn,
+              offsetY: 20,
+              font: "15px sans-serif",
+            }),
+          }),
+          new Style({
+            image: new Circle({
+              radius: 8 + feature.getProperties().antallelever / 80,
+              stroke: new Stroke({ color: "black", width: 5 }),
+              fill: new Fill({
+                color:
+                  feature.getProperties().eierforhold === "Offentlig"
+                    ? "blue"
+                    : "purple",
+              }),
+            }),
+          }),
+        ];
+      },
+    }),
   ],
 });
 
 export function Application() {
+  function handlePointerMove(e: MapBrowserEvent<MouseEvent>) {
+    activeSchools.clear(true);
+    const newActive = map.getFeaturesAtPixel(e.pixel, {
+      layerFilter: (layer) => layer === schoolLayer,
+    });
+    for (const active of newActive) {
+      const geometry = active.getGeometry()!.clone() as Geometry;
+      activeSchools.addFeature(
+        new Feature({ geometry, properties: active.getProperties() }),
+      );
+    }
+    activeSchools.refresh();
+  }
+
   const mapRef = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
     map.setTarget(mapRef.current!);
+    map.on("pointermove", handlePointerMove);
   }, []);
 
   return <div ref={mapRef}></div>;
