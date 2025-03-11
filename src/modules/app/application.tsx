3c99031809ddaf2e34e0a270d075e6d88f0af6f5
Johannes Brodwall <johannes@brodwall.com>
2025-03-11 12:42:41 +0100

    DrawPointButton

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -5,6 +5,7 @@ import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
 
 import "ol/ol.css";
+import { Draw } from "ol/interaction";
 
 useGeographic();
 
@@ -13,10 +14,27 @@ const map = new Map({
   layers: [new TileLayer({ source: new OSM() })],
 });
 
+interface DrawPointButtonProps {
+  map: Map;
+}
+
+function DrawPointButton({ map }: DrawPointButtonProps) {
+  function handleClick() {
+    map.addInteraction(new Draw({ type: "Point" }));
+  }
+
+  return <button onClick={handleClick}>Add point</button>;
+}
+
 export function Application() {
   const mapRef = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
     map.setTarget(mapRef.current!);
   }, []);
-  return <div ref={mapRef}></div>;
+  return (
+    <>
+      <DrawPointButton map={map} />
+      <div ref={mapRef}></div>
+    </>
+  );
 }
