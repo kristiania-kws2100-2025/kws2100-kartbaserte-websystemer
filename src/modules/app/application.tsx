327eac834704288d0a0572b6925c8f580c625368
Johannes Brodwall <johannes@brodwall.com>
2025-03-11 12:50:35 +0100

    style the points (good enough for now)

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -8,6 +8,7 @@ import "ol/ol.css";
 import { Draw } from "ol/interaction";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
+import { Circle, Fill, Stroke, Style } from "ol/style";
 
 useGeographic();
 
@@ -16,7 +17,24 @@ const map = new Map({
   view: new View({ center: [10.8, 59.9], zoom: 13 }),
   layers: [
     new TileLayer({ source: new OSM() }),
-    new VectorLayer({ source: drawingVectorSource }),
+    new VectorLayer({
+      source: drawingVectorSource,
+      style: [
+        new Style({
+          image: new Circle({
+            radius: 10,
+            stroke: new Stroke({ color: "white", width: 2 }),
+            fill: new Fill({ color: "blue" }),
+          }),
+        }),
+        new Style({
+          image: new Circle({
+            radius: 12,
+            stroke: new Stroke({ color: "black", width: 2 }),
+          }),
+        }),
+      ],
+    }),
   ],
 });
 
