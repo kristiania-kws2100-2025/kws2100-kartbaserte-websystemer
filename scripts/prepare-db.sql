89660023713f58c408340aa14348b2197ccc1d4e
Johannes Brodwall <johannes@brodwall.com>
2025-03-23 12:26:08 +0100

    simplify sql by preprocessing data in PostgreSQL

---
+++ scripts/prepare-db.sql
@@ -0,0 +1,13 @@
+drop table if exists fylke_2023;
+create table fylke_2023
+as
+select fylke.omrade, navn.navn
+from fylker_ba7aea2735714391a98b1a585644e98a.fylke fylke
+         inner join fylker_ba7aea2735714391a98b1a585644e98a.administrativenhetnavn navn
+                    on fylke.objid = navn.fylke_fk and navn.sprak = 'nor';
+
+drop table if exists grunnskole;
+create table grunnskole
+as
+select *, st_transform(posisjon, 4326) as posisjon_4326
+from grunnskoler_3697913259634315b061b324a3f2cf59.grunnskole;
--- server/server.ts
+++ server/server.ts
@@ -15,13 +15,10 @@ app.get("/api/hello", async (c) => {
 });
 app.get("/api/skoler", async (c) => {
   const result = await postgresql.query(`
-      select skolenavn, st_transform(skole.posisjon, 4326)::json as posisjon
-      from grunnskoler_3697913259634315b061b324a3f2cf59.grunnskole skole
-               inner join fylker_ba7aea2735714391a98b1a585644e98a.fylke fylke
-                          on st_contains(fylke.omrade, skole.posisjon)
-               inner join fylker_ba7aea2735714391a98b1a585644e98a.administrativenhetnavn navn
-                          on fylke.objid = navn.fylke_fk and navn.sprak = 'nor'
-      where navn.navn = 'Viken'
+      select skolenavn, skole.posisjon_4326::json as posisjon
+      from grunnskole skole inner join fylke_2023
+          on st_contains(fylke_2023.omrade, skole.posisjon)
+      where fylke_2023.navn = 'Viken'
   `);
   return c.json({
     type: "FeatureCollection",
