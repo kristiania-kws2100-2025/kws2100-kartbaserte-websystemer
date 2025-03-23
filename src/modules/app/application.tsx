d36e3bbe85fe688accd091352551ad518f9f6903
Johannes Brodwall <johannes@brodwall.com>
2025-03-23 13:40:53 +0100

    show overlays on clicked feature

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -4,11 +4,14 @@ import { useGeographic } from "ol/proj";
 import "ol/ol.css";
 import TileLayer from "ol/layer/Tile";
 import { StadiaMaps } from "ol/source";
-import { Map, View } from "ol";
+import { Map, Overlay, View } from "ol";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
 import { Fill, Style } from "ol/style";
+import { FeatureLike } from "ol/Feature";
+
+import "./application.css";
 
 useGeographic();
 
@@ -28,10 +31,39 @@ const schoolProximityLayer = new VectorLayer({
   },
 });
 const map = new Map({ view: new View({ center: [10.8, 59.9], zoom: 12 }) });
+const overlay = new Overlay({ positioning: "bottom-center" });
+map.addOverlay(overlay);
+
+function OverlayDiv({ features }: { features: FeatureLike[] }) {
+  return (
+    <>
+      <h3>{features.length} features</h3>
+      {features
+        .map((f) => f.getProperties())
+        .map(({ geometry, ...props }, index) => (
+          <div key={index}>
+            <pre>{JSON.stringify(props, null, 2)}</pre>
+          </div>
+        ))}
+    </>
+  );
+}
 
 export function Application() {
   const mapRef = useRef<HTMLDivElement | null>(null);
-  useEffect(() => map.setTarget(mapRef.current!), []);
+  const overlayRef = useRef<HTMLDivElement | null>(null);
+  const [selectedFeatures, setSelectedFeatures] = useState<FeatureLike[]>([]);
+  useEffect(() => {
+    map.setTarget(mapRef.current!);
+    overlay.setElement(overlayRef.current!);
+    map.on("click", (e) => {
+      const selectedFeatures = map.getFeaturesAtPixel(e.pixel);
+      setSelectedFeatures(selectedFeatures);
+      overlay.setPosition(
+        selectedFeatures.length > 0 ? e.coordinate : undefined,
+      );
+    });
+  }, []);
 
   const [layers, setLayers] = useState([
     osmLayer,
@@ -40,5 +72,13 @@ export function Application() {
   ]);
   useEffect(() => map.setLayers(layers), [layers]);
 
-  return <div ref={mapRef} />;
+  return (
+    <div ref={mapRef}>
+      <div ref={overlayRef}>
+        {selectedFeatures.length > 0 && (
+          <OverlayDiv features={selectedFeatures} />
+        )}
+      </div>
+    </div>
+  );
 }
