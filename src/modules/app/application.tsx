dba9742df8edde35abdacd34d921d0ca97a5aa48
Johannes Brodwall <johannes@brodwall.com>
2025-03-23 13:22:42 +0100

    style school proximity zones

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -3,20 +3,24 @@ import { useGeographic } from "ol/proj";
 
 import "ol/ol.css";
 import TileLayer from "ol/layer/Tile";
-import { OSM } from "ol/source";
+import { StadiaMaps } from "ol/source";
 import { Map, View } from "ol";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
+import { Fill, Style } from "ol/style";
 
 useGeographic();
 
-const osmLayer = new TileLayer({ source: new OSM() });
+const osmLayer = new TileLayer({
+  source: new StadiaMaps({ layer: "stamen_toner_lite" }),
+});
 const schoolLayer = new VectorLayer({
   source: new VectorSource({ url: "/api/skoler", format: new GeoJSON() }),
 });
 const schoolProximityLayer = new VectorLayer({
   source: new VectorSource({ url: "/api/naerskoler", format: new GeoJSON() }),
+  style: new Style({ fill: new Fill({ color: "#88ff8844" }) }),
 });
 const map = new Map({ view: new View({ center: [10.8, 59.9], zoom: 12 }) });
 
