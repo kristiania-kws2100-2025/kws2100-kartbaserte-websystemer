2841ba446ad715a4a2adf7d4134815e63176485b
Johannes Brodwall <johannes@brodwall.com>
2025-03-23 12:17:28 +0100

    read /api/skoler

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -5,17 +5,23 @@ import "ol/ol.css";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 import { Map, View } from "ol";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
 
 useGeographic();
 
 const osmLayer = new TileLayer({ source: new OSM() });
+const schoolLayer = new VectorLayer({
+  source: new VectorSource({ url: "/api/skoler", format: new GeoJSON() }),
+});
 const map = new Map({ view: new View({ center: [10.8, 59.9], zoom: 11 }) });
 
 export function Application() {
   const mapRef = useRef<HTMLDivElement | null>(null);
   useEffect(() => map.setTarget(mapRef.current!), []);
 
-  const [layers, setLayers] = useState([osmLayer]);
+  const [layers, setLayers] = useState([osmLayer, schoolLayer]);
   useEffect(() => map.setLayers(layers), [layers]);
 
   return <div ref={mapRef} />;
