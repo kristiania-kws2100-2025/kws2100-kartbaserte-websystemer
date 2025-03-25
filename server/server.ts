b92b535aeeb3f55b65287bcc3bf41a34a20a6fea
Johannes Brodwall <johannes@brodwall.com>
2025-03-25 12:59:06 +0100

    return schools from /api/schools

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,19 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+import pg from "pg";
 
 const app = new Hono()
 
-app.get("/api/hello", c => {
-  return c.json({"hello": "class"})
+const postgres = new pg.Pool({
+  user: "postgres",
+  password: "mitt_eget_passord"
+});
+
+
+app.get("/api/schools", async (c) => {
+  const result = await postgres.query(
+    "select skolenavn, organisasjonsnummer, lavestetrinn, hoyestetrinn, antallelever from grunnskole")
+  return c.json(result.rows)
 })
 
 serve(app);
