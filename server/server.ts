5e19a406a38dd98b11c091e0185be7eaa6a60727
Johannes Brodwall <johannes@brodwall.com>
2025-03-25 12:55:19 +0100

    create basic Hono server

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,10 @@
-console.log("Hello world");
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+
+const app = new Hono()
+
+app.get("/api/hello", c => {
+  return c.json({"hello": "class"})
+})
+
+serve(app);
