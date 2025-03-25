236de206f000349b187077e1b3b88816d427c0dc
Johannes Brodwall <johannes@brodwall.com>
2025-03-25 13:20:47 +0100

    add typescript, prettier and husky

--- server/server.ts
+++ server/server.ts
@@ -2,18 +2,18 @@ import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import pg from "pg";
 
-const app = new Hono()
+const app = new Hono();
 
 const postgres = new pg.Pool({
   user: "postgres",
-  password: "mitt_eget_passord"
+  password: "mitt_eget_passord",
 });
-
 
 app.get("/api/schools", async (c) => {
   const result = await postgres.query(
-    "select skolenavn, organisasjonsnummer, lavestetrinn, hoyestetrinn, antallelever from grunnskole")
-  return c.json(result.rows)
-})
+    "select skolenavn, organisasjonsnummer, lavestetrinn, hoyestetrinn, antallelever from grunnskole",
+  );
+  return c.json(result.rows);
+});
 
 serve(app);
