ecc134b827a420eb8b46ae5b1289cdae9ca8f093
Johannes Brodwall <johannes@brodwall.com>
2025-04-01 13:35:18 +0200

    return schools as feature collection as an Hono API to Postgis

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,31 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
+import pg from "pg";
+
+const connectionString = process.env.DATABASE_URL;
+const postgresql = connectionString
+  ? new pg.Pool({ connectionString, ssl: { rejectUnauthorized: false } })
+  : new pg.Pool({ user: "postgres", password: "postgres" });
 
 const app = new Hono();
+app.get("/api/schools", async (c) => {
+  const result = await postgresql.query(
+    "select skolenavn, st_transform(posisjon, 4326)::json as geometry from grunnskoler_3697913259634315b061b324a3f2cf59.grunnskole",
+  );
+  return c.json({
+    type: "FeatureCollection",
+    crs: { type: "name", properties: { name: "ESPG:4326" } },
+    features: result.rows.map(
+      ({ skolenavn, geometry: { coordinates, type } }) => ({
+        type: "Feature",
+        geometry: { type, coordinates },
+        properties: { skolenavn },
+      }),
+    ),
+  });
+});
+
 app.use("*", serveStatic({ root: "../dist" }));
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
