35cda5604be97c8496b7dd1530f71d9aa4d8bae3
Johannes Brodwall <johannes@brodwall.com>
2025-04-01 19:26:45 +0200

    show overlay

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -1,24 +1,19 @@
 import React, { useEffect, useRef } from "react";
-import { Map, View } from "ol";
+import { Map, Overlay, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
 
-// Styling of OpenLayers components like zoom and pan controls
 import "ol/ol.css";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
 
-// By calling the "useGeographic" function in OpenLayers, we tell that we want coordinates to be in degrees
-//  instead of meters, which is the default. Without this `center: [10.6, 59.9]` brings us to "null island"
+import "./application.css";
+
 useGeographic();
 
-// Here we create a Map object. Make sure you `import { Map } from "ol"`. Otherwise, the standard Javascript
-//  map data structure will be used
 const map = new Map({
-  // The map will be centered on a position in longitude (x-coordinate, east) and latitude (y-coordinate, north),
-  //   with a certain zoom level
   view: new View({ center: [10.8, 59.9], zoom: 13 }),
   layers: [
     new TileLayer({ source: new OSM() }),
@@ -37,16 +32,24 @@ const map = new Map({
   ],
 });
 
-// A functional React component
 export function Application() {
-  // `useRef` bridges the gap between JavaScript functions that expect DOM objects and React components
   const mapRef = useRef<HTMLDivElement | null>(null);
-  // When we display the page, we want the OpenLayers map object to target the DOM object refererred to by the
-  // map React component
+  const overlayRef = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
+    const overlay = new Overlay({
+      element: overlayRef.current!,
+      positioning: "bottom-center",
+    });
     map.setTarget(mapRef.current!);
+    map.addOverlay(overlay);
+    map.on("click", (e) => {
+      overlay.setPosition(e.coordinate);
+    });
   }, []);
 
-  // This is the location (in React) where we want the map to be displayed
-  return <div ref={mapRef}></div>;
+  return (
+    <div ref={mapRef}>
+      <div ref={overlayRef}>Overlay</div>
+    </div>
+  );
 }
