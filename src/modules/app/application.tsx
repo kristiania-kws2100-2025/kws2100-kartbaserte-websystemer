44d6fef7b382328d6ec3032e6d6b72786b5a234c
Johannes Brodwall <johannes@brodwall.com>
2025-04-01 19:37:33 +0200

    hide overlay on click

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Map, Overlay, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
@@ -10,31 +10,34 @@ import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
 
 import "./application.css";
+import { FeatureLike } from "ol/Feature";
 
 useGeographic();
 
+const osmLayer = new TileLayer({ source: new OSM() });
+const schoolLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/api/schools",
+    format: new GeoJSON(),
+  }),
+});
+const coverageLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/api/grunnkretser",
+    format: new GeoJSON(),
+  }),
+});
 const map = new Map({
   view: new View({ center: [10.8, 59.9], zoom: 13 }),
-  layers: [
-    new TileLayer({ source: new OSM() }),
-    new VectorLayer({
-      source: new VectorSource({
-        url: "/api/schools",
-        format: new GeoJSON(),
-      }),
-    }),
-    new VectorLayer({
-      source: new VectorSource({
-        url: "/api/grunnkretser",
-        format: new GeoJSON(),
-      }),
-    }),
-  ],
+  layers: [osmLayer, schoolLayer, coverageLayer],
 });
 
 export function Application() {
   const mapRef = useRef<HTMLDivElement | null>(null);
   const overlayRef = useRef<HTMLDivElement | null>(null);
+
+  const [activeAreas, setActiveAreas] = useState<FeatureLike[]>([]);
+
   useEffect(() => {
     const overlay = new Overlay({
       element: overlayRef.current!,
@@ -43,13 +46,35 @@ export function Application() {
     map.setTarget(mapRef.current!);
     map.addOverlay(overlay);
     map.on("click", (e) => {
-      overlay.setPosition(e.coordinate);
+      const features = map.getFeaturesAtPixel(e.pixel, {
+        layerFilter: (l) => l === coverageLayer,
+      });
+      setActiveAreas((old) => {
+        if (old.length > 0) {
+          overlay.setPosition(undefined);
+          return [];
+        } else {
+          overlay.setPosition(features.length > 0 ? e.coordinate : undefined);
+          return features;
+        }
+      });
     });
   }, []);
 
   return (
     <div ref={mapRef}>
-      <div ref={overlayRef}>Overlay</div>
+      <div ref={overlayRef}>
+        <h2>Overlay</h2>
+        <pre>
+          {JSON.stringify(
+            activeAreas
+              .map((f) => f.getProperties())
+              .map(({ geometry, ...properties }) => properties),
+            null,
+            2,
+          )}
+        </pre>
+      </div>
     </div>
   );
 }
