75dbcc354e4f388f1221dce42497350a415d4262
Johannes Brodwall <johannes@brodwall.com>
2025-04-01 12:58:57 +0200

    create server for Heroku

---
+++ server/server.ts
@@ -0,0 +1,9 @@
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+
+const app = new Hono();
+app.use("*", serveStatic({ root: "../dist" }));
+
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+serve({ fetch: app.fetch, port });
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")!).render(<h1>Hello React</h1>);
+createRoot(document.getElementById("root")!).render(
+  <h1>Hello React on Heroku</h1>,
+);
